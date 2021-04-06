904a1d1190b8d89f9773af40c0b881f93cc8efa2
JoeTanto2 <77851879+JoeTanto2@users.noreply.github.com>
2021-04-06 17:29:46 +0300

    s
    s

--- website/models.py
+++ website/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Quiz (models.Model):
-    question = models.ForeignKey('Question')
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
     answer = models.CharField (max_length=150)
 
 class Question (models.Model):
     question = models.CharField(max_length=200)
+
 
 
--- website/views.py
+++ website/views.py
@@ -7,15 +7,4 @@ def home(request):
 def cities(request):
     return render(request, 'website/cities.html')
 
-questions = ['Do you like modern Skyscrapers or prefer old architecture?', 'Would you ratehr go clubbing or visit a museum?', 'Do you like sports?',
-'Do you speak English or prefer to speak other languages?', 'Do you enjoy nature-made wonders?']
-
-answers = [['I like SkyScrapers', 'I enjoy old architecture more', 'I would go to a city that has both'],
-           ['I would rather go clubbing', 'I find museums a better place to spend my time at'],
-           ['Yes, I love sports', 'Not really'],
-           ['I want to speak English ONLY', 'I do speak English and I like learning new languages too'],
-           ['who does not?', 'I do enjoy nature, but I am more interested in men-made objects', 'Meh']
-                                                                                                           ]
-def questions():
-    for my_questions, my_answers in zip(questions, answers)
 
