56822ae68a7b69fc40953055c36481e96827de4c
JoeTanto2 <77851879+JoeTanto2@users.noreply.github.com>
2021-04-06 17:04:45 +0300

    s
    s

--- website/models.py
+++ website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quiz (models.Model):
+    question = models.ForeignKey('Question')
+    answer = models.CharField (max_length=150)
+
+class Question (models.Model):
+    question = models.CharField(max_length=200)
+
+
