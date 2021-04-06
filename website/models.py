b0b27888bffaf0a942bbf045dc7a64c4e5eeaea7
JoeTanto2 <77851879+JoeTanto2@users.noreply.github.com>
2021-04-06 18:20:00 +0300

    DB
    DB

--- website/models.py
+++ website/models.py
@@ -7,5 +7,8 @@ class Quiz (models.Model):
 class Question (models.Model):
     question = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.question
+
 
 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
 def home(request):
-    return render(request, 'website/home.html')
+    context = {
+        'questions': Question.objects.all()
+    }
+    return render(request, 'website/home.html', context)
 
 def cities(request):
     return render(request, 'website/cities.html')
